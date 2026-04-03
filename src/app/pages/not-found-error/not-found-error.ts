1e41cc49d6536b27c163156bf1cab159a65caa6a
PablinkiDev <agusdev.fernandez@bue.edu.ar>
2026-04-03 17:26:38 -0300

    ADD: Ruteo

---
+++ src/app/pages/not-found-error/not-found-error.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-error',
+  imports: [],
+  templateUrl: './not-found-error.html',
+})
+export class NotFoundError {}
