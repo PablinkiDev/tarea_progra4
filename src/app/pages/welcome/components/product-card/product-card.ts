788dfbf286f4081823fa055a22c73697219dc0ca
PablinkiDev <agusdev.fernandez@bue.edu.ar>
2026-04-04 17:49:30 -0300

    ADD: Welcome Page FULL

---
+++ src/app/pages/welcome/components/product-card/product-card.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+})
+export class ProductCard {}
--- src/app/pages/welcome/welcome.ts
+++ src/app/pages/welcome/welcome.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { Topics } from './components/topics/topics';
+import { ProductCard } from './components/product-card/product-card';
 
 @Component({
   selector: 'app-welcome',
-  imports: [Hero, Topics],
+  imports: [Hero, Topics, ProductCard],
   templateUrl: './welcome.html',
 })
 export class Welcome {}
