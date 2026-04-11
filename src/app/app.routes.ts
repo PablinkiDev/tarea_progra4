3b3637cf68aae4e49c38c5cfae5d4706d77917a9
PablinkiDev <agusdev.fernandez@bue.edu.ar>
2026-04-11 01:29:09 -0300

    ADD: Profile Path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
     { path: 'welcome', loadComponent: () => import('./pages/welcome/welcome').then((m) => m.Welcome) },
     { path: '', redirectTo: 'welcome', pathMatch: 'full'},
     { path: 'about-me', loadComponent: () => import('./pages/about-me/about-me').then((m) => m.AboutMe) },
+    { path: 'profile', loadComponent: () => import('./pages/profile/profile').then((m) => m.Profile) },
     { path: 'login', loadComponent: () => import('./pages/auth/login/login').then((m) => m.Login) },
     { path: 'register', loadComponent: () => import('./pages/auth/register/register').then((m) => m.Register) },
     { path: '**', pathMatch: 'full', loadComponent: () => import('./pages/not-found-error/not-found-error').then((m) => m.NotFoundError) },
